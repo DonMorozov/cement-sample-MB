1a55b5e879a9bc94bc775022f2f1ba9b81baf05c
aa.morozov <aa.morozov@skbkontur.ru>
2022-01-18 16:13:00 +0500

    Hello, world!

---
+++ cement-sample-MB/cement-sample-MB/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cement_sample_MB
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("It is MB cement-sample project!");
+        }
+    }
+}
